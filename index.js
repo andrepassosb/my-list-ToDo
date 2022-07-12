079dd02f8f7c08183196797aecaa33f7b36130b3
andrepassosb <andre.passosb@gmail.com>
2022-07-11 22:28:21 -0300

    Changes for deploy

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const app = express()
 app.use(express.json())
 
 //Solve CORS
-app.use(cors({ credentials: true, origin:'http://localhost:3000'}))
+// app.use(cors({ credentials: true, origin:'http://localhost:3000'}))
 
 
 //Routes
 const UserRoutes = require('./routes/UserRoutes')
 const ListRoutes = require('./routes/ListRoutes')
 // const StandardListRoutes = require('./routes/StandardListRoutes')
+
+app.get('/',(req, res) =>{
+    return res.json({message:"Serves is up!"})
+})
 
 app.use('/user',UserRoutes)
 app.use('/list',ListRoutes)
