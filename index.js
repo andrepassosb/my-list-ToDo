352dfeacb871b42be8e2b84f9face0086421d68d
André Passos <63965710+andrepassosb@users.noreply.github.com>
2022-07-19 17:26:45 -0300

    update cors

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 //Routes
 const UserRoutes = require("./routes/UserRoutes");
