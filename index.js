e83cd236624f6876739cf64a6b095e35a5de31a0
andrepassosb <andre.passosb@gmail.com>
2022-07-18 23:32:14 -0300

    allow all cors

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ const app = express();
 app.use(express.json());
 
 //Solve CORS
-var allowedOrigins = [
-  "http://localhost:3000",
-  "http://naya-caju-list.vercel.app",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+// var allowedOrigins = [
+//   "http://localhost:3000",
+//   "http://naya-caju-list.vercel.app",
+// ];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors());
 
 //Routes
 const UserRoutes = require("./routes/UserRoutes");
