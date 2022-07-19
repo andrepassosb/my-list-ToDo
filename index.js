818162cc744831f685aa952255b5fc0a48ccc843
andrepassosb <andre.passosb@gmail.com>
2022-07-18 23:05:45 -0300

    change cors

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require("express")
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 //Config json response
-app.use(express.json())
+app.use(express.json());
 
 //Solve CORS
-app.use(cors({ credentials: true, origin:'http://localhost:3000'}))
-
+app.use(
+  cors({ credentials: true, origin: "https://naya-caju-list.vercel.app" })
+);
 
 //Routes
-const UserRoutes = require('./routes/UserRoutes')
-const ListRoutes = require('./routes/ListRoutes')
+const UserRoutes = require("./routes/UserRoutes");
+const ListRoutes = require("./routes/ListRoutes");
 // const StandardListRoutes = require('./routes/StandardListRoutes')
 
-app.get('/',(req, res) =>{
-    return res.json({message:"Serves is up!"})
-})
+app.get("/", (req, res) => {
+  return res.json({ message: "Serves is up!" });
+});
 
-app.use('/user',UserRoutes)
-app.use('/list',ListRoutes)
+app.use("/user", UserRoutes);
+app.use("/list", ListRoutes);
 // app.use('/standardlist',StandardListRoutes)
 
-app.listen(8000)
+app.listen(8000);
