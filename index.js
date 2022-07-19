5eb3823646f703cb38967fd29a4c13bcaa161db7
andrepassosb <andre.passosb@gmail.com>
2022-07-18 23:49:57 -0300

    cors origin

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ app.use(express.json());
 //   })
 // );
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 //Routes
 const UserRoutes = require("./routes/UserRoutes");
